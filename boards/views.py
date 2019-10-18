a71b6bfa6dd11d328c4c142b712296c6c97bb121
ntirnanich <ntirnanich@yahoo.com>
2019-10-18 07:43:12 -0500

    pt2 VIEWS, TEMPLATES

--- boards/views.py
+++ boards/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Board
 
 def home(request):
-    return HttpResponse('Hello, World!')
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
+
+    
+
+
+    from django.http import HttpResponse
